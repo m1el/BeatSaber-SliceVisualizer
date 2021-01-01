93fe4c7b810e30dfa4a9b4f2b8b01d58862a94d2
Igor null <m1el.2027@gmail.com>
2021-01-01 18:54:05 +0200

    cleanup unnecessary using

--- Assets.cs
+++ Assets.cs
@@ -1,10 +1,5 @@
-﻿using IPA;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 using IPALogger = IPA.Logging.Logger;
 
--- Plugin.cs
+++ Plugin.cs
@@ -1,15 +1,8 @@
 ﻿using IPA;
 using IPA.Config;
 using IPA.Config.Stores;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using IPALogger = IPA.Logging.Logger;
-using IPA.Config;
-using IPA.Config.Stores;
 
 namespace SliceVisualizer
 {
